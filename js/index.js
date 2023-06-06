05880472ebd385b82e502d703193a8a5ef37ea34
kevinstivencortes <kevinstivencortesmaigual36@gmail.com>
2023-06-05 19:01:42 -0500

    add cambios

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function guardar(){
+    let valorp1, valorp2, valorp3
+    valorp1 = document.getElementById("p1").value
+    //guardar localmente
+    localStorage.setItem("p1",valorp1)
+    //enviar al archivo respuesta.html   
+    window.location="respuesta.html"
+
+    valorp2 = document.getElementById("p2").value
+    //guardar localmente
+    localStorage.setItem("p2",valorp2)
+    //enviar al archivo respuesta.html   
+    window.location="respuesta.html"
+
+    valorp3 = document.getElementById("p3").value
+    /* guardar localmente */
+    localStorage.setItem("p3", valorp3)
+    /* enviar al archivo respuesta.html */
+    window.location="respuesta.html"
+}
+
+
+
